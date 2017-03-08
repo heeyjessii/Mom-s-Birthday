b1ff0b8bbce48673da2d67726c0dff1411d28431
11lopez.jessica@gmail.com <11lopezjessica@gmail.com>
2017-03-07 22:04:55 -1000

    oops

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function reasons(){
+  console.log('hi');
+   document.getElementById("button").style.color = "blue";
+
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode("BECAUSE I JUST LOVE YOU DUHHHH"));
+  ul.appendChild(li);
+  
+}
